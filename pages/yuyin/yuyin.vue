f9d78c21e939e4592b07bb793662ee6e97401e72
陈欣培 <c_moon_824@163.com>
2022-11-16 16:00:31 +0800

    语音

--- pages/yuyin/yuyin.vue
+++ pages/yuyin/yuyin.vue
@@ -31,46 +31,54 @@
 			let self = this;
 			recorderManager.onStop(function(res) {
 				self.text = 'recorder stop' + JSON.stringify(res)
-				console.log(res.tempFilePath);
-				plus.io.resolveLocalFileSystemURL(res.tempFilePath, entry => {
-					entry.file(file => {
-						const fileReader = new plus.io.FileReader()
-						fileReader.readAsDataURL(file)
-						fileReader.onloadend = e => {
-							self.voicePath = e.target.result
-						}
-						// fileReader.close()
-					})
-				})
+				// console.log(res.tempFilePath);
+				const path = plus.io.convertLocalFileSystemURL(res.tempFilePath)
+				// console.log(path,'path');
+				const fileReader = new plus.io.FileReader()
+				fileReader.readAsDataURL(path)
+				fileReader.onloadend = res => {
+					// console.log(res.target.result.split(',')[1],'res');
+					self.voicePath = res.target.result.split(',')[1]
+				}
+				// plus.io.resolveLocalFileSystemURL(res.tempFilePath, entry => {
+				// 	entry.file(file => {
+				// 		const fileReader = new plus.io.FileReader()
+				// 		fileReader.readAsDataURL(file)
+				// 		fileReader.onloadend = e => {
+				// 			self.voicePath = e.target.result
+				// 		}
+				// 		// fileReader.close()
+				// 	})
+				// })
 				// self.voicePath = res.tempFilePath;
 			});
 		},
 		methods: {
 			startRecord() {
-				console.log('开始录音');
+				// console.log('开始录音');
 				recorderManager.start({
 					sampleRate: '44100'
 				});
 			},
 			endRecord() {
-				console.log('录音结束');
+				// console.log('录音结束');
 				recorderManager.stop();
 			},
 			playVoice() {
-				console.log('播放录音');
+				// console.log('播放录音');
 				if (this.voicePath) {
 					innerAudioContext.src = this.voicePath;
 					innerAudioContext.play();
 				}
 			},
 			upload() {
-				this.searchText = this.voicePath.split('base64,')[1]
-				// console.log(this.searchText);
+				this.searchText = this.voicePath
+				console.log(this.searchText);
 				uni.request({
 					url: "http://192.168.1.49:8090/paddlespeech/asr",
 					method: 'POST',
 					data: {
-						"audio": "exSI6ICJlbiIsCgkgICAgInBvc2l0aW9uIjogImZhbHNlIgoJf...",
+						"audio": this.voicePath,
 						"audio_format": "wav",
 						"sample_rate": 16000,
 						"lang": "zh_cn",
